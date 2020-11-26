233786a5665897516ebac4912ad5e761eda1fe36
DiyaGupta <73223496+DiyaGupta@users.noreply.github.com>
2020-11-26 16:03:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+	//Create the Bodies Here.
+	
+		//Create a Ground
+	ground = new Ground(400,680,800,20)
+	 side1=new Box(500,650,200,20)
+	side2=new Box(400,600,20,100)
+	side3=new Box(600,600,20,100)
+     paper=new Paper(100,630,20)
+
+
+
+	Engine.run(engine);
+
+	
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  side1.display();
+  side2.display();
+  side3.display();
+  ground.display();
+   paper.display();
+
+
+ 
+}
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+Matter.Body.applyForce(paper.body,paper.body.position,{x:40,y:-40})
+	}
+}
+   
+
+
+
